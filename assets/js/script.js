b0e25299620afb18c986f6b91cecb762135d3a75
EdsonZuchi <edsonluiz.zuchi@gmail.com>
2022-11-06 19:22:59 -0300

    animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,11 @@ function game(play1, hour, play2) {
   `
 }
 
+let delay = -0.3;
 function createCard(date, day, game) {
+  delay += +0.3; 
   return `
-        <div class="card">
+        <div class="card" style="animation-delay: ${delay}s">
           <h2>${date} <span>${day}</span></h2>
           <ul>
             ${game}
@@ -19,28 +21,24 @@ function createCard(date, day, game) {
   `
 }
 
-document.querySelector("#app").innerHTML = `
-      <header>
-        <img src="assets/img/logo.svg" alt="logo" />
-      </header>
-      <main id="cards">
-        ${createCard(
-          "24/11",
-          "quinta",
-          game("switzerland", "07:00", "cameroon") +
-            game("brazil", "16:00", "Serbia")
-        )}
-        ${createCard(
-          "28/11",
-          "segunda",
-          game("cameroon", "07:00", "serbia") +
-            game("brazil", "13:00", "switzerland")
-        )}
-        ${createCard(
-          "02/12",
-          "sexta",
-          game("serbia", "16:00", "switzerland") +
-            game("brazil", "16:00", "cameroon")
-        )}
-      </main>
-`
+document.querySelector("#cards").innerHTML +=
+  createCard(
+    "24/11",
+    "quinta",
+    game("switzerland", "07:00", "cameroon") +
+    game("brazil", "16:00", "Serbia")
+  )
+  +
+  createCard(
+    "28/11",
+    "segunda",
+    game("cameroon", "07:00", "serbia") +
+    game("brazil", "13:00", "switzerland")
+  )
+  +
+  createCard(
+    "02/12",
+    "sexta",
+    game("serbia", "16:00", "switzerland") +
+    game("brazil", "16:00", "cameroon")
+  )
